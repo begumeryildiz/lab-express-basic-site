497ac5d73d5a8494e0638fdd8e49dca28747c433
Paul Tiry <tirypaul@gmail.com>
2022-07-05 18:28:28 +0200

    making work page

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 // Require Express
-const express = require('express')
+const express = require("express");
 
 // Express server handling requests and responses
-const app = express()
+const app = express();
 
 // Make everything inside of public/ available
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'))
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
 
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'))
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
 
-app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'))
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
 
 // Server Started
-app.listen(3001, () => console.log('My first app listening on port 3000!'))
+app.listen(3001, () => console.log("My first app listening on port 3001!"));
